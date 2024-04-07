0351aad7a1b4747d9c9bec90ff1deeeddb5bb5ef
pacasiano <peterandre.casiano@Icloud.com>
2024-04-07 12:29:13 +0800

    test

--- Frontend/components/calendar/calendar.jsx
+++ Frontend/components/calendar/calendar.jsx
@@ -1,6 +1,6 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
 import Topbar from '../general/topbar'
 import PropTypes from 'prop-types';
 
